f4f3db8471e5d20828f69e2cab1db7fd0b991263
Grigory <0064777@gmail.com>
2021-03-17 20:53:47 +0300

    pz Grigory Klimov lesson 1

--- task_1_1.py
+++ task_1_1.py
@@ -1,9 +1,11 @@
-duration = int(input('Введите количество секунд: '))
-sec = duration % 60
-min = duration // 60
-res_min = min
-hour = duration // 3600
-day = duration // 86400
-remains_hour = duration % 86400 // 3600
-remains_min = duration % 86400 % 3600 // 60
-print('Получилось количество {}дней {}часов {}минут {}секунд'.format(day, remains_hour, remains_min, sec))
+while True:
+    duration = (input('Введите количество секунд: '))
+    if duration.isdigit():
+        duration = int(duration)
+        if duration > 0:
+            sec = duration % 60
+            day = duration // 86400
+            remains_hour = duration % 86400 // 3600
+            remains_min = duration % 86400 % 3600 // 60
+            print('Получилось количество {}дней {}часов {}минут {}секунд'.format(day, remains_hour, remains_min, sec))
+            break
--- task_1_2.py
+++ task_1_2.py
@@ -1,12 +1,34 @@
-numbers = []
-i = 1
-final_number = []
-while i <= 1000:
-    numbers.append(i)
-    i += 2
-    numbers_3 = i**3
-    final_number.append(numbers_3)
-    if final_number[-1] >=1000:
-        break
-print(numbers)
-print(final_number)
+
+cubic_numbers = []                                          #создаю список кубов
+odd_numbers = list(range(1, 1000, 2))                       #создаю список нечетных цифр
+for cubic_number in odd_numbers:
+    cubic_number **=3
+    cubic_numbers.append(cubic_number)
+#print(cubic_numbers)                                        #вывел для проверки
+good_number_list = []                                       #создал список нечетных кубов которые всецело делятся на 7
+number_list = []                                            #создал временный список для сортировки
+for number in cubic_numbers:                                #беру каждый элемент списка
+    number_x = number
+    while number_x > 0:                                     #циклом раскладываю его на составляющие
+        number_list.append(number_x % 10)
+        number_x //= 10
+    if sum(number_list) % 7 == 0:                           #сверяюсь с условиями и при удовлетворении добавляю в список правильных цифр
+        good_number_list.append(number)
+    number_list.clear()
+print(sum(good_number_list))                                #вывожу сумму правильных чисел по задаче 1
+i = len(cubic_numbers)
+while i != 0:                                               #добавляю "17" к каждому элементу списка
+    i -= 1
+    cubic_numbers[i] += 17
+#print(cubic_numbers)
+i = len(cubic_numbers)
+while i >= 0:                                               #создаю цикл для редактирования и сортировки элементов списка(не создавая новый список, как указано в задании)
+    i -= 1
+    number_x = cubic_numbers[i]
+    while number_x > 0:
+        number_list.append(number_x % 10)
+        number_x //= 10
+    if sum(number_list) % 7 != 0:
+        del cubic_numbers[i]
+    number_list.clear()
+print(sum(cubic_numbers))                                   #вывожу сумму правильных чисел по задаче 2
